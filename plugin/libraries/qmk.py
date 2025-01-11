33a1096bef427ed6f05484aca2c20d4af504ecbb
cibere <71997063+cibere@users.noreply.github.com>
2025-01-10 17:18:09 -0800

    update qmk library to dynamically get index url

--- plugin/libraries/qmk.py
+++ plugin/libraries/qmk.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
+import asyncio
 from typing import TYPE_CHECKING, ClassVar
 
+import bs4
 import msgspec
 from yarl import URL
 
@@ -22,9 +24,6 @@ class QmkLocalSearchData(msgspec.Struct):
 
 
 class QmkDocs(Library):
-    inventory_url: ClassVar[str] = (
-        "https://docs.qmk.fm/assets/chunks/@localSearchIndexroot.DuIlbnO1.js"
-    )
     classname: ClassVar[str] = "docs.qmk.fm"
     is_preset: ClassVar[bool] = True
     favicon_url: ClassVar[str] | None = "https://docs.qmk.fm"
@@ -32,9 +31,50 @@ class QmkDocs(Library):
     def __init__(self, name: str, *, use_cache: bool) -> None:
         super().__init__(name, URL("https://docs.qmk.fm/"), use_cache=use_cache)
 
-    async def build_cache(self, session: ClientSession, webserver_port: int) -> None:
-        async with session.get(self.inventory_url) as res:
-            raw_content: bytes = await res.content.read()
+    def qmk_get_theme(self, text: bytes) -> list[str]:
+        soup = bs4.BeautifulSoup(text.decode(), "html.parser")
+        return [tag.attrs["href"] for tag in soup.find_all("link", rel="modulepreload")]
+
+    async def qmk_parse_theme_file(
+        self, session: ClientSession, url: str
+    ) -> str | None:
+        assert isinstance(self.loc, URL)
+
+        async with session.get(self.loc.with_path(url)) as res:
+            raw_content = await res.content.read()
+        data = raw_content.decode()
+        if not data.startswith("const __vite__fileDeps=["):
+            return
+        data = data.removeprefix('const __vite__fileDeps=["').split('"')[0]
+        assert "VPLocalSearchBox" in data
+        return data
+
+    async def qmk_parse_search_box_file(
+        self, session: ClientSession, url: str
+    ) -> str | None:
+        assert isinstance(self.loc, URL)
+
+        async with session.get(self.loc.with_path(url)) as res:
+            raw_content = await res.content.read()
+        data = raw_content.decode()
+
+        for line in data.splitlines():
+            if not line.startswith(
+                'const localSearchIndex = { "root": () => __vitePreload(() => import'
+            ):
+                continue
+
+            return line.removeprefix(
+                'const localSearchIndex = { "root": () => __vitePreload(() => import(".'
+            ).split('"')[0]
+
+    async def parse_index(
+        self, session: ClientSession, filename: str, webserver_port: int
+    ) -> dict[str, str]:
+        assert isinstance(self.loc, URL)
+
+        async with session.get(self.loc / "assets" / "chunks" / filename) as res:
+            raw_content = await res.content.read()
 
         line = raw_content.splitlines()[0].decode()
         raw_json = (
@@ -50,4 +90,25 @@ class QmkDocs(Library):
             document = index.documentIds[docid]
             cache[field.title] = self._build_url(document, webserver_port)
 
-        self.cache = cache
+        return cache
+
+    async def build_cache(self, session: ClientSession, webserver_port: int) -> None:
+        assert isinstance(self.loc, URL)
+
+        async with session.get(self.loc) as res:
+            raw_content: bytes = await res.content.read()
+        tags = await asyncio.to_thread(self.qmk_get_theme, raw_content)
+
+        for tag in tags:
+            search_box_url = await self.qmk_parse_theme_file(session, tag)
+            print(f"{search_box_url=}")
+            if search_box_url is None:
+                continue
+            index_name = await self.qmk_parse_search_box_file(session, search_box_url)
+            print(f"{index_name=}")
+            if index_name is None:
+                continue
+            self.cache = await self.parse_index(
+                session, index_name.strip("/"), webserver_port
+            )
+            return
--- test.py
+++ test.py
@@ -1,20 +1,16 @@
 
 import aiohttp
 import json
-from plugin.libraries.qmk import QmkIndexReader
+from plugin.libraries.qmk import QmkDocs
 
 
 async def main() -> None:
     async with aiohttp.ClientSession() as cs:
-        async with cs.get("https://docs.qmk.fm/assets/chunks/@localSearchIndexroot.DuIlbnO1.js") as res:
-            reader = QmkIndexReader(await res.content.read())
-        
-        data = reader.parse_file()
-        with open("data.json", "w", encoding="UTF-8") as f:
-            f.write(data)
-        print(json.loads(data))
-        with open("data.json", "w", encoding="UTF-8") as f:
-            json.dump(json.loads(data), f, indent=4)
+        docs = QmkDocs("d", use_cache=True)
+        await docs.build_cache(cs, 0)
+    
+    with open("cache.json", "w") as f:
+        json.dump(docs.cache, f, indent=4)
 
 
 import asyncio
