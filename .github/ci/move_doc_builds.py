ddefc1ec658f24bee2ae26c9fe3620ab1d61752b
cibere <71997063+cibere@users.noreply.github.com>
2025-01-13 14:34:23 -0800

    Update move_doc_builds.py

--- .github/ci/move_doc_builds.py
+++ .github/ci/move_doc_builds.py
@@ -1,23 +1,24 @@
 import os
 from pathlib import Path
 
+ci_dir = Path(__file__)
+root_dir = Path(*ci_dir.parts[0:-3])
+builds_dir = root_dir / "docs" / "_build"
 
-def main():
-    ci_dir = Path(__file__)
-    root_dir = Path(*ci_dir.parts[0:-3])
-    builds_dir = root_dir / "docs" / "_build"
 
-    for file in builds_dir.rglob("*"):
-        if file.name.endswith((".pickle", ".doctree", ".doctrees")):
-            file.unlink()
-            continue
-
-        relative_fp = Path(str(file).removeprefix(str(builds_dir)))
+def move_item(path: Path):
+    if path.is_dir():
+        for child in path.rglob("*"):
+            if child.name.endswith((".pickle", ".doctree", ".doctrees")):
+                continue
+            move_item(child)
+    else:
+        relative_fp = Path(str(path).removeprefix(str(builds_dir)))
 
         dest_fp = Path(*root_dir.parts + relative_fp.parts[1:]).resolve()
         print(f"Moving {relative_fp} to {dest_fp}")
-        os.rename(file, dest_fp)
+        os.rename(path, dest_fp)
 
 
 if __name__ == "__main__":
-    main()
+    move_item(builds_dir)
