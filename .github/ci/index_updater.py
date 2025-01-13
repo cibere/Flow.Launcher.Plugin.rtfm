455aca4ee1a92f333c0fd6039f780c646b7deecd
cibere <71997063+cibere@users.noreply.github.com>
2025-01-12 20:30:53 -0800

    move doc index auto updator to main branch

---
+++ .github/ci/index_updater.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+import os
+import re
+import jinja2
+
+VERSION_RE = re.compile(
+    r"(?P<major>[0-9]+)\.(?P<minor>[0-9]+)\.(?P<micro>[0-9]+)(?:(?P<level>[ab])(?P<serial>[0-9]*))?"
+)
+
+
+def main():
+    ci_dir = Path(os.path.dirname(__file__))
+    root_dir = Path(*ci_dir.parts[:-2])
+
+    index_fp = root_dir / "index.html"
+
+    versions = [
+        folder.name
+        for folder in root_dir.glob("*")
+        if folder.is_dir and VERSION_RE.fullmatch(folder.name)
+    ]
+
+    env = jinja2.Environment(loader=jinja2.PackageLoader("update_index", ""))
+    template = env.get_template("index_template.html")
+
+    with index_fp.open("w", encoding="UTF-8") as f:
+        f.write(template.render(versions=versions))
+
+
+if __name__ == "__main__":
+    main()
