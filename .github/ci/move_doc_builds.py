8eb274e345fb6c6d45608720831438c8275b0689
cibere <71997063+cibere@users.noreply.github.com>
2025-01-12 17:11:59 -0800

    Update move_doc_builds.py

--- .github/ci/move_doc_builds.py
+++ .github/ci/move_doc_builds.py
@@ -5,10 +5,10 @@ def main():
     ci_dir = Path(__file__)
     root_dir = Path(*ci_dir.parts[0:-3])
     builds_dir = root_dir / "docs" / "_build"
-    print(root_dir.parts)
+    #print(root_dir.parts)
     for file in builds_dir.rglob("*"):
         relative_fp = Path(str(file).removeprefix(str(builds_dir)))
-        print(relative_fp.parts)
+        #print(relative_fp.parts)
         dest_fp = Path(*root_dir.parts+ relative_fp.parts[1:]).resolve()
         print(f"Moving {relative_fp} to {dest_fp}")
         os.rename(file, dest_fp)
