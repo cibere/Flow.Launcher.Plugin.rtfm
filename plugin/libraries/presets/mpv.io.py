25b08509f70d1b3bcdfc9bebef2c697c86f805c8
cibere <71997063+cibere@users.noreply.github.com>
2025-01-26 18:34:39 -0800

    use the rtfm indexes for mpv.io

--- plugin/libraries/presets/mpv.io.py
+++ plugin/libraries/presets/mpv.io.py
@@ -1,57 +1,22 @@
 from __future__ import annotations
 
-import asyncio
-import re
-from functools import partial
-from typing import TYPE_CHECKING, Callable
-
-import bs4
-
-from plugin.libraries.preset import PresetLibrary
-
-if TYPE_CHECKING:
-    from aiohttp import ClientSession
+from plugin.libraries.rtfm_index import RtfmIndexBase
 
 
-class MpvIoParser:
-    def __init__(self, data: bytes, url_builder: Callable[[str], str]) -> None:
-        self.data = data
-        self.soup = bs4.BeautifulSoup(data, "html.parser")
-        self.cache: dict[str, str] = {}
-        self.url_builder = url_builder
-
-    def header_to_fragment(self, header: str) -> str:
-        return "#" + header.lower().replace(" ", "-")
-
-    def parse(
-        self,
-    ) -> None:
-        headers: list[bs4.Tag] = self.soup.find_all(re.compile("^h[1-6]$"))
-
-        for header in headers:
-            self.cache[header.text] = self.url_builder(
-                self.header_to_fragment(header.text)
-            )
+class MpvIoMaster(
+    RtfmIndexBase,
+    name="mpv.io-master",
+    url="https://mpv.io/manual/master/",
+    is_variant=True,
+): ...
 
 
-class MpvIoBase(PresetLibrary, is_variant=True):
-    def __init_subclass__(cls, version: str) -> None:
-        return super().__init_subclass__(base_url=f"https://mpv.io/manual/{version}")
-
-    async def build_cache(self, session: ClientSession, webserver_port: int) -> None:
-        async with session.get(self.url) as res:
-            raw_content: bytes = await res.content.read()
-
-        parser = MpvIoParser(raw_content, partial(self._build_url, port=webserver_port))
-
-        await asyncio.to_thread(parser.parse)
-        self.cache = parser.cache
+class MpvIoStable(
+    RtfmIndexBase,
+    name="mpv.io-stable",
+    url="https://mpv.io/manual/stable/",
+    is_variant=True,
+): ...
 
 
-class MpvIoLatest(MpvIoBase, version="master"): ...
-
-
-class MpvIoStable(MpvIoBase, version="stable"): ...
-
-
-presets = (MpvIoLatest, MpvIoStable)
+presets = (MpvIoMaster, MpvIoStable)
