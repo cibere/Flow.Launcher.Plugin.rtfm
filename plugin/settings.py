85e8428900f001f399fae2008d318b0324224c17
cibere <71997063+cibere@users.noreply.github.com>
2025-02-26 12:14:35 -0800

    fix settings legacy converters

--- plugin/settings.py
+++ plugin/settings.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import asyncio
+import logging
 from collections import defaultdict
 from typing import TYPE_CHECKING, Any
 
@@ -11,6 +12,7 @@ from rtfm_lookup import IndexerName, PartialManual
 if TYPE_CHECKING:
     from .plugin import RtfmPlugin
 
+log = logging.getLogger(__name__)
 
 # region Current
 
@@ -25,14 +27,26 @@ class RtfmBetterSettings(msgspec.Struct, tag="3.0", tag_field="version"):
 
     @classmethod
     def decode(cls, data: bytes | str) -> RtfmBetterSettings:
-        try:
-            return v3_decoder.decode(data)
-        except msgspec.DecodeError as error:
+        raw = dict_decoder.decode(data)
+        version = raw.get("version", None)
+        if version == "3.0":
+            return current_decoder.decode(data)
+
+        log.debug(
+            "Settings version not current, running legacy decoders. Version: %r",
+            version,
+        )
+        for decoder in legacy_decoders:
             try:
-                settings = v2_decoder.decode(data)
-            except msgspec.DecodeError:
-                raise error from None
+                settings = decoder.decode(data)
+            except msgspec.DecodeError as e:
+                log.debug("Given settings does not fit %r", decoder.type, exc_info=e)
+                continue
+
             return settings.convert()
+
+        log.error("Failed to decode settings. Raw Data: %r\nSerialized: %r", data, raw)
+        raise ValueError("Could not match data to a decoder")
 
     def encode(self) -> bytes:
         return encoder.encode(self)
@@ -69,13 +83,43 @@ class RtfmBetterSettings(msgspec.Struct, tag="3.0", tag_field="version"):
         return cls(**kwargs)
 
     async def save(self, plugin: RtfmPlugin) -> None:
+        log.debug(
+            "Saving static port. %r -> %r",
+            plugin.better_settings.static_port,
+            self.static_port,
+        )
         plugin.better_settings.static_port = self.static_port
+
+        log.debug(
+            "Saving main kw. %r -> %r", plugin.better_settings.main_kw, self.main_kw
+        )
         plugin.better_settings.main_kw = self.main_kw
+
+        log.debug(
+            "Saving simple_view. %r -> %r",
+            plugin.better_settings.simple_view,
+            self.simple_view,
+        )
         plugin.better_settings.simple_view = self.simple_view
+
+        log.debug(
+            "Saving reset_query. %r -> %r",
+            plugin.better_settings.reset_query,
+            self.reset_query,
+        )
         plugin.better_settings.reset_query = self.reset_query
+
+        log.debug(
+            "Saving manuals. %r -> %r", plugin.better_settings.manuals, self.manuals
+        )
         plugin.better_settings.manuals = self.manuals
 
         if self.debug_mode != plugin.better_settings.debug_mode:
+            log.debug(
+                "Saving debug mode. %r -> %r",
+                plugin.better_settings.debug_mode,
+                self.debug_mode,
+            )
             plugin.logs.update_debug(self.debug_mode)
             plugin.better_settings.debug_mode = self.debug_mode
 
@@ -92,18 +136,16 @@ class PartialLibrary(msgspec.Struct):
     name: str
     type: str
     loc: str
-    dont_cache_results: bool = False
+    use_cache: bool = False
     is_api: bool = False
 
 
-class RtfmBetterSettingsV2(msgspec.Struct):
-    main_kw: str = "rtfm"
-    static_port: int = 0
-    libraries: list[PartialLibrary] = []
-    version: str | None = None
-    debug_mode: bool = False
-    simple_view: bool = False
-    reset_query: bool = False
+class LegacySettingsV2(msgspec.Struct):
+    main_kw: str
+    static_port: int
+    libraries: list[PartialLibrary]
+    debug_mode: bool
+    simple_view: bool
 
     def convert(self) -> RtfmBetterSettings:
         return RtfmBetterSettings(
@@ -111,7 +153,6 @@ class RtfmBetterSettingsV2(msgspec.Struct):
             static_port=self.static_port,
             debug_mode=self.debug_mode,
             simple_view=self.simple_view,
-            reset_query=self.reset_query,
             manuals=[
                 PartialManual(
                     name=lib.name,
@@ -125,7 +166,41 @@ class RtfmBetterSettingsV2(msgspec.Struct):
                     ),
                     loc=lib.loc,
                     options={
-                        "dont_cache_results": lib.dont_cache_results,
+                        "dont_cache_results": not lib.use_cache,
+                    },
+                )
+                for lib in self.libraries
+            ],
+        )
+
+
+class LegacySettingsV1(msgspec.Struct):
+    keyword: str
+    port: int
+    libraries: list[PartialLibrary]
+    debug: bool
+    simple: bool
+
+    def convert(self) -> RtfmBetterSettings:
+        return RtfmBetterSettings(
+            main_kw=self.keyword,
+            static_port=self.port,
+            debug_mode=self.debug,
+            simple_view=self.simple,
+            manuals=[
+                PartialManual(
+                    name=lib.name,
+                    type=IndexerName(
+                        {
+                            "Preset": "cibere-rtfm-indexes",
+                            "Gidocgen": "gidocgen",
+                            "Intersphinx": "intersphinx",
+                            "Mkdocs": "mkdocs",
+                        }[lib.type]
+                    ),
+                    loc=lib.loc,
+                    options={
+                        "dont_cache_results": not lib.use_cache,
                     },
                 )
                 for lib in self.libraries
@@ -136,5 +211,9 @@ class RtfmBetterSettingsV2(msgspec.Struct):
 # region json
 
 encoder = msgspec.json.Encoder()
-v3_decoder = msgspec.json.Decoder(RtfmBetterSettings)
-v2_decoder = msgspec.json.Decoder(RtfmBetterSettingsV2)
+current_decoder = msgspec.json.Decoder(RtfmBetterSettings)
+dict_decoder = msgspec.json.Decoder(type=dict)
+legacy_decoders = (
+    msgspec.json.Decoder(type=LegacySettingsV1),
+    msgspec.json.Decoder(type=LegacySettingsV2),
+)
